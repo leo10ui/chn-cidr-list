cff8223cd2fc60c0f9af566a9875f930bb4d8d05
fernvenue <git@fernvenue.com>
2022-08-21 14:58:26 +0000

    Update tools/cidr-merger.license, tools/cidr2mmdb, tools/cidr-merger, README.md, updater.sh

--- updater.sh
+++ updater.sh
@@ -8,34 +8,31 @@ git config --global user.email "$GIT_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
 git clone git@gitlab.com:fernvenue/chn-cidr-list.git
 cd "./chn-cidr-list"
-curl "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt" > ipv4-bgp.txt
-sed -i "s/[[:space:]]//g" "./ipv4-bgp.txt"
-curl "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv4-apnic.txt
-sed -i "s/[[:space:]]//g" "./ipv4-apnic.txt"
-chmod +x ./cidr-merger
-cat ./ipv4*.txt | ./cidr-merger -s > ./ipv4.txt
-cp ./ipv4.txt ./ipv4.yaml
-sed -i "s|^|  - '&|g" ./ipv4.yaml
-sed -i "s|$|&'|g" ./ipv4.yaml
-sed -i "1s|^|payload:\n|" ./ipv4.yaml
-cp ./ipv4.txt ./ipv4.conf
-sed -i "s|^|IP-CIDR,|g" ./ipv4.conf
-curl "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt" > ipv6-bgp.txt
-sed -i "s/[[:space:]]//g" "./ipv6-bgp.txt"
-curl "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" | grep '|CN|ipv6|' | cut -d'|' -f'4,5' | tr '|' '/' > ipv6-apnic.txt
-sed -i "s/[[:space:]]//g" "./ipv6-apnic.txt"
-cat ./ipv6*.txt | ./cidr-merger -s > ./ipv6.txt
-cp ./ipv6.txt ./ipv6.yaml
-sed -i "s|^|  - '&|g" ./ipv6.yaml
-sed -i "s|$|&'|g" ./ipv6.yaml
-sed -i "1s|^|payload:\n|" ./ipv6.yaml
-cp ./ipv6.txt ./ipv6.conf
-sed -i "s|^|IP-CIDR,|g" ./ipv6.conf
-chmod +x ./cidr2mmdb
-./cidr2mmdb -i ./ipv4.txt -o ./ipv4.mmdb
-./cidr2mmdb -i ./ipv6.txt -o ./ipv6.mmdb
-rm cidr-merger *bgp.txt *apnic.txt
+curl "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt" > bgp-ipv4.list
+curl "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt" > bgp-ipv6.list
+curl "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > apnic-ipv4.list
+curl "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" | grep '|CN|ipv6|' | cut -d'|' -f'4,5' | tr '|' '/' > apnic-ipv6.list
+sed -i "s/[[:space:]]//g" ./*.list
+chmod +x ./tools/cidr-merger
+cat ./*ipv4.list | ./tools/cidr-merger -s > ./ipv4.txt
+cat ./*ipv6.list | ./tools/cidr-merger -s > ./ipv6.txt
+cp ./ipv4.txt ./ipv4.conf && cp ./ipv4.txt ./ipv4.yaml
+cp ./ipv6.txt ./ipv6.conf && cp ./ipv6.txt ./ipv6.yaml
+sed -i "s|^|IP-CIDR,|g" ./*.conf
+sed -i "s|^|  - '&|g" ./*.yaml
+sed -i "s|$|&'|g"  ./*.yaml
+sed -i "1s|^|payload:\n|" ./*.yaml
+rm ./*.mmdb
 updated=`date --rfc-3339 sec`
+sed -i "1i # GitHub: https://github.com/fernvenue/chn-cidr-list" ./ipv*
+sed -i "1i # GitLab: https://gitlab.com/fernvenue/chn-cidr-list" ./ipv*
+sed -i "1i # Updated: $updated" ./ipv*
+sed -i "1i # License: BSD-3-Clause License" ./ipv*
+sed -i "1i # CHN CIDR list" ./ipv*
+chmod +x ./tools/cidr2mmdb
+./tools/cidr2mmdb -i ./ipv4.txt -o ./ipv4.mmdb
+./tools/cidr2mmdb -i ./ipv6.txt -o ./ipv6.mmdb
+rm *.list
 git init
 git add .
 git commit -m "$updated"
